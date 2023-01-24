8f6b2673931f53ca98a276589369c4fce815c711
Viacheslav Kolarzh <guin-s@yandex.ru>
2023-01-24 12:39:29 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,18 @@ public class Main {
         int adjunction = 50;
         int bonus;
 
-        if (adjunction>=100){
-            bonus=adjunction/100;}
-        else{
-            bonus=0;
+        if (adjunction > 100) {
+            bonus = adjunction / 100;
+        } else {
+            bonus = 0;
         }
 
-        int newBalance=currentBalance + adjunction +bonus;
+        int newBalance = currentBalance + adjunction + bonus;
 
-        System.out.println("начальный баланс: "+ currentBalance + " руб.");
-        System.out.println("сумма пополнения: "+ adjunction + " руб.");
-        System.out.println("сумма бонуса: "+ bonus + " руб.");
-        System.out.println("новый баланс: "+ newBalance + " руб.");
-
-
+        System.out.println("начальный баланс: " + currentBalance + " руб.");
+        System.out.println("сумма пополнения: " + adjunction + " руб.");
+        System.out.println("сумма бонуса: " + bonus + " руб.");
+        System.out.println("новый баланс: " + newBalance + " руб.");
 
 
     }
